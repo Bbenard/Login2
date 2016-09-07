1883733e51003332431b015508d8b0e47504e1e5
Bbenard <benkip69@gmail.com>
2016-09-07 15:26:05 +0300

    entire login commit

--- app/src/main/java/com/example/benard/login/LoginActivity.java
+++ app/src/main/java/com/example/benard/login/LoginActivity.java
@@ -1,21 +1,91 @@
 package com.example.benard.login;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonsignin;
     private EditText editTextEmail;
     private EditText editTextPassword;
     //private EditText textEmailAddress;
     private TextView textviewsignup;
+    //private progressDialog progressDialog;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        buttonsignin =(Button) findViewById (R.id.buttonsignin);
+        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        editTextEmail=(EditText)findViewById(R.id.editTextPassword);
+        textviewsignup=(TextView)findViewById(R.id.textviewsignin);
+        progressDialog=new ProgressDialog(this);
+        firebaseAuth= FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser()!=null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+
+        }
+
+        buttonsignin.setOnClickListener(this);
+        textviewsignup.setOnClickListener(this);
+
+    }
+    private void userLogin(){
+        String email=editTextEmail.getText().toString().trim();
+        String password=editTextPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(email )){
+            Toast.makeText(this,"Please Enter your email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            if (TextUtils.isEmpty(password )){
+                Toast.makeText(this,"Please Enter your email",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        progressDialog.setMessage("Register user");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Registred succesfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Could not Register Succesfully", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view==buttonsignin){
+            userLogin();
+        }
+        else if (view==textviewsignup){
+            finish();
+            startActivity(new Intent(this,MainActivity.class) );
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/benard/login/ProfileActivity.java
@@ -0,0 +1,48 @@
+package com.example.benard.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+ private FirebaseAuth firebaseAuth;
+ private TextView textViewUserMail;
+ private Button buttonlogout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        buttonlogout=(Button)findViewById(R.id.buttonlogout);
+        textViewUserMail=(TextView)findViewById(R.id.textviewsignin);
+        buttonlogout=(Button)findViewById(R.id.buttonlogout);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        buttonlogout.setOnClickListener(this);
+        if (firebaseAuth.getCurrentUser()==null){
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+
+        }
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        textViewUserMail=(TextView)findViewById(R.id.textviewsignin);
+        textViewUserMail.setText("welcome"+user.getEmail());
+
+
+        }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
